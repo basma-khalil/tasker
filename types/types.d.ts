53c6777f02c02c23ad68ee894b4f161f013eecf6
Basma Khalil <basma.mahmoud.khalil@gmail.com>
2024-11-05 14:47:45 +0200

    chore: Add types declaration

---
+++ types/types.d.ts
@@ -0,0 +1,11 @@
+interface ITask {
+  id: string;
+  date: string;
+  title: string;
+  description?: string;
+  priority: 'low' | 'medium' | 'high';
+  state: 'todo' | 'doing' | 'done';
+  image: string;
+}
+
+type TTasks = ITask[];
